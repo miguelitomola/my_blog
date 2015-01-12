fa2b4aa14f7ff595020bce4757ad6f868ee7d9bc
Miguel AML <mangelmontero@hotmail.com>
2015-01-12 14:35:44 +0100

    crud and links

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get 'contact' => 'staticpages#contact'
 
+  resources :articles
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20150108173221) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "article_number"
