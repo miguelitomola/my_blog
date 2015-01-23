f86b973596812de91c24d994802c6861d0052435
Miguel AML <mangelmontero@hotmail.com>
2015-01-23 16:59:08 +0100

    Restricted access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper :all
   helper_method :current_user_session, :current_user
-  filter_parameter_logging :password, :password_confirmation
 
   private
     def current_user_session
@@ -16,5 +14,30 @@ class ApplicationController < ActionController::Base
     def current_user
       @current_user = current_user_session && current_user_session.record
     end
+
+    def require_user
+      logger.debug "ApplicationController::require_user"
+      unless current_user
+        store_location
+        flash[:notice] = "You must be logged in to access this page"
+        redirect_to new_user_session_url
+        return false
+      end
+    end
+  
+    def require_no_user
+      logger.debug "ApplicationController::require_no_user"
+      if current_user
+        store_location
+        flash[:notice] = "You must be logged out to access this page"
+        redirect_to account_url
+        return false
+    end
+    end
+  
+    def store_location
+      session[:return_to] = request.url
+    end
 end
+  
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+	before_action :require_user, only: [:new, :edit, :create, :update]
+
 	def index
   	@articles = Article.all
   end
