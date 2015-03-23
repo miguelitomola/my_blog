54631b1809e13e84996ba38642afb954d3c1296b
Miguel AML <miganml@outlook.com>
2015-03-23 21:14:32 +0100

    window.onload

--- app/assets/javascripts/aside_dates.js
+++ app/assets/javascripts/aside_dates.js
@@ -1,7 +1,7 @@
-var years = document.getElementsByClassName('year');
-
 //When doing click on a year, show months
-for (var i=0; i<years.length; i++) {
+var years = document.getElementsByClassName('year');
+//When doing click on a year, show months
+for (var i=0; i<years.length; i+=1){
   //Function needs event as parameter to work
   years[i].onclick = function(event){
     var selectedYear = event.target;
@@ -12,7 +12,31 @@ for (var i=0; i<years.length; i++) {
     //Hiding months
     }else{
     month_list.classList.add('hide_months');
-    };
+    }
   };
 };
+
+// window.onload = function(){
+//   var years = document.getElementsByClassName('year');
+//   //When doing click on a year, show months
+//   for (var i=0; i<years.length; i+=1){
+//     //Function needs event as parameter to work
+//     years[i].onclick = function(event){
+//       var selectedYear = event.target;
+//       var month_list = selectedYear.nextSibling.nextSibling;
+//       //Showing months
+//       if (month_list.classList.contains('hide_months')){
+//         month_list.classList.remove('hide_months');
+//       //Hiding months
+//       }else{
+//         month_list.classList.add('hide_months');
+//       }
+//     };
+//   };
+// }
+
+// Code from StackOverFlow: 
+// function myHandler(){...}
+// window.onload = myHandler;
+// myElement.onclick= myHandler;
 
--- app/assets/javascripts/nav_slide.js
+++ app/assets/javascripts/nav_slide.js
@@ -1,19 +1,20 @@
 // On general nav bar, contact button must slide a sub menu 
 
-
-var contactBtn = document.getElementsByClassName("contact_button")[0];
-var subMenu = document.getElementsByClassName("sub_nav")[0];
-
-function ShowSubMenu () {
-	subMenu.classList.remove("display_none");
+window.onload = function(){
+  var contactBtn = document.getElementsByClassName("contact_button")[0];
+  var subMenu = document.getElementsByClassName("sub_nav")[0];
+  
+  function ShowSubMenu () {
+  	subMenu.classList.remove("display_none");
+  };
+  
+  function HideSubMenu () {
+  	subMenu.classList.add("display_none");
+  };
+  
+  contactBtn.addEventListener('mouseover', ShowSubMenu, false);
+  contactBtn.addEventListener('mouseout', HideSubMenu, false);
 };
-
-function HideSubMenu () {
-	subMenu.classList.add("display_none");
-};
-
-contactBtn.addEventListener('mouseover', ShowSubMenu);
-contactBtn.addEventListener('mouseout', HideSubMenu);
 
 
 
